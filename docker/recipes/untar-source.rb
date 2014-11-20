1d47532e6f7d6099a229f5c4f6c69b6a3043191f
Kyrylo Novotarskyi <kirill.novotarsky@happycar.de>
2014-11-20 15:23:03 +0100

    Initial commit with docker and util recipes

---
+++ docker/recipes/untar-source.rb
@@ -0,0 +1,9 @@
+execute "untar-code" do
+  cwd "#{deploy[:deploy_to]}"
+  command "for a in `ls -1 *.tar.gz`; do tar -zxvf $a; done"
+end
+
+execute "cleanup-mess" do
+  cwd "#{deploy[:deploy_to]}"
+  command "mv temporary/* " + #{deploy[:deploy_to]}
+end
