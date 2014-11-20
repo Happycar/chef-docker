857ef644060c1379de1775d37dc4063f3f004baf
Kyrylo Novotarskyi <kirill.novotarsky@happycar.de>
2014-11-20 17:42:17 +0100

    Comments for debug

--- docker/recipes/untar-source.rb
+++ docker/recipes/untar-source.rb
@@ -1,5 +1,5 @@
 include_recipe 'deploy'
-
+Chef::Log.debug("The recipe actuallz starts")
 node[:deploy].each do |application, deploy|
 
   if node[:opsworks][:instance][:layers].first != deploy[:environment_variables][:layer]
@@ -18,11 +18,15 @@ node[:deploy].each do |application, deploy|
     app application
   end
 
+  Chef::Log.debug("Before untar")
+
   bash "untar-code" do
     cwd "#{deploy[:deploy_to]}/current"
     command "for a in `ls -1 *.tar.gz`; do tar -zxvf $a; done"
   end
 
+  Chef::Log.debug("Before file moving")
+  
   bash "cleanup-mess" do
     cwd "#{deploy[:deploy_to]}/current"
     command "mv temporary/* #{deploy[:deploy_to]}/current"
