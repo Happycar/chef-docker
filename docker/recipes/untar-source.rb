ca02daff393f263e4d856a1940f01d55d3490b89
Kyrylo Novotarskyi <kirill.novotarsky@happycar.de>
2014-11-20 15:51:33 +0100

    Deploy for all instances

--- docker/recipes/untar-source.rb
+++ docker/recipes/untar-source.rb
@@ -1,9 +1,31 @@
-execute "untar-code" do
-  cwd "#{deploy[:deploy_to]}"
-  command "for a in `ls -1 *.tar.gz`; do tar -zxvf $a; done"
+node[:deploy].each do |application, deploy|
+
+  if node[:opsworks][:instance][:layers].first != deploy[:environment_variables][:layer]
+    Chef::Log.debug("Skipping deploy::docker application #{application} as it is not deployed to this layer")
+    next
+  end
+
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
+
+  bash "untar-code" do
+    cwd "#{deploy[:deploy_to]}/current"
+    command "for a in `ls -1 *.tar.gz`; do tar -zxvf $a; done"
+  end
+
+  bash "cleanup-mess" do
+    cwd "#{deploy[:deploy_to]}/current"
+    command "mv temporary/* #{deploy[:deploy_to]}/current"
+  end
+
 end
 
-execute "cleanup-mess" do
-  cwd "#{deploy[:deploy_to]}"
-  command "mv temporary/* #{deploy[:deploy_to]}"
-end
+
