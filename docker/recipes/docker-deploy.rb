8443b66802503761c600f6e072492f35226d4deb
Kyrylo Novotarskyi <kirill.novotarsky@happycar.de>
2014-11-21 11:50:40 +0100

    Run only if there are images ready to run

--- docker/recipes/docker-deploy.rb
+++ docker/recipes/docker-deploy.rb
@@ -59,7 +59,10 @@ node[:deploy].each do |application, deploy|
     user "root"
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
-      docker run #{dockerenvs} -p #{node[:opsworks][:instance][:private_ip]}:#{deploy[:environment_variables][:service_port]}:#{deploy[:environment_variables][:container_port]} --name #{deploy[:application]} -v '#{deploy[:environment_variables][:host_code_path]}':#{deploy[:environment_variables][:docker_mount_path]} -d #{deploy[:application]}
+      if docker images | grep #{deploy[:application]};
+      then
+        docker run #{dockerenvs} -p #{node[:opsworks][:instance][:private_ip]}:#{deploy[:environment_variables][:service_port]}:#{deploy[:environment_variables][:container_port]} --name #{deploy[:application]} -v '#{deploy[:environment_variables][:host_code_path]}':#{deploy[:environment_variables][:docker_mount_path]} -d #{deploy[:application]}
+      fi
     EOH
   end
 
