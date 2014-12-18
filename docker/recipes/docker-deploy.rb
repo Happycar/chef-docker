a34549e79c4eab0e89b043420d91c33d68efd46c
Kyrylo Novotarskyi <kirill.novotarsky@happycar.de>
2014-12-18 11:25:47 +0100

    срус

--- docker/recipes/docker-deploy.rb
+++ docker/recipes/docker-deploy.rb
@@ -27,8 +27,6 @@ node[:deploy].each do |application, deploy|
    bash "give-rights-to-deploy" do
          user "root"
          code <<-EOH
-          rm -rf /var/www/
-          mkdir -p /var/www/
           chown www-data:www-data /var/www/
          EOH
        end
