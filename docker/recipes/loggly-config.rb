6def10eeacb16824f2962b148cbdd88db4a28b2b
Kyrylo Novotarskyi <kirill.novotarsky@happycar.de>
2015-07-02 14:40:34 +0200

    loggly config

---
+++ docker/recipes/loggly-config.rb
@@ -0,0 +1,10 @@
+execute 'loggly-config-download' do
+  command 'curl -O https://www.loggly.com/install/configure-linux.sh'
+end
+
+bash "run-config" do
+    user "root"
+    code <<-EOH
+        configure-linux.sh -a node['loggly']['subdomain'] -t node['loggly']['token'] -p node['loggly']['password'] -u node['loggly']['username']
+    EOH
+  end
