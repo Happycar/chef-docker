480471f12843cf5117027131cd35030b5ae33522
Kyrylo Novotarskyi <kirill.novotarsky@happycar.de>
2015-07-02 14:59:37 +0200

    path

--- docker/recipes/loggly-config.rb
+++ docker/recipes/loggly-config.rb
@@ -5,6 +5,6 @@ end
 bash "run-config" do
     user "root"
     code <<-EOH
-        configure-linux.sh -a node['loggly']['subdomain'] -t node['loggly']['token'] -p node['loggly']['password'] -u node['loggly']['username']
+        /configure-linux.sh -a node['loggly']['subdomain'] -t node['loggly']['token'] -p node['loggly']['password'] -u node['loggly']['username']
     EOH
   end
