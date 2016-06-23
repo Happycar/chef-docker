93652af3cc23e867f51aa9b18411cf47237dc4f1
a.kostiuk <alexey.kostiuk@happycar.de>
2016-06-23 14:57:31 +0200

    fix docker-compose deploy path

--- docker/recipes/docker-compose-deploy.rb
+++ docker/recipes/docker-compose-deploy.rb
@@ -4,6 +4,11 @@ Chef::Log.info("Entering docker-compose-deploy")
 
 node[:deploy].each do |application, deploy|
 
+  if node[:opsworks][:instance][:layers].first != deploy[:environment_variables][:layer]
+    Chef::Log.debug("Skipping deploy::docker application #{application} as it is not deployed to this layer")
+    next
+  end
+
   opsworks_deploy_dir do
     user deploy[:user]
     group deploy[:group]
@@ -15,26 +20,28 @@ node[:deploy].each do |application, deploy|
     app application
   end
 
-  Chef::Log.info('docker-login start')
-  bash "docker-login" do
-    user "root"
-    code <<-EOH
-      docker login -u #{deploy[:environment_variables][:DOCKER_HUB_USER]} -p #{deploy[:environment_variables][:DOCKER_HUB_PASSWORD]}
-    EOH
-  end
-  Chef::Log.info('docker-login stop')
+  if (deploy[:environment_variables]) 
+    Chef::Log.info('docker-login start')
+    bash "docker-login" do
+      user "root"
+      code <<-EOH
+        docker login -u #{deploy[:environment_variables][:DOCKER_HUB_USER]} -p #{deploy[:environment_variables][:DOCKER_HUB_PASSWORD]}
+      EOH
+    end
+    Chef::Log.info('docker-login stop')
 
-  Chef::Log.info('docker-compose-run start')
-  bash "docker-run" do
-    user "root"
-    code <<-EOH
-      echo "start composer"
-      export PRIVATE_IP=#{node[:opsworks][:instance][:private_ip]}
-      printenv
-      docker-compose -f #{deploy[:deploy_to]}/current/docker-compose.yml down
-      docker-compose -f #{deploy[:deploy_to]}/current/docker-compose.yml up -d
-    EOH
+    Chef::Log.info('docker-compose-run start')
+    bash "docker-compose-run" do
+      user "root"
+      code <<-EOH
+        export PRIVATE_IP=#{node[:opsworks][:instance][:private_ip]}
+        docker-compose -f #{deploy[:deploy_to]}/current/docker-compose.yml pull
+        docker-compose -f #{deploy[:deploy_to]}/current/docker-compose.yml down
+        docker-compose -f #{deploy[:deploy_to]}/current/docker-compose.yml up -d
+      EOH
+    end
   end
+  
 
 end
 Chef::Log.info("Exiting docker-compose-deploy")
