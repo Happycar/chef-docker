b38d3099894b1c152aa4c8b62793ca7b0b03da66
a.kostiuk <alexey.kostiuk@happycar.de>
2016-07-04 17:47:29 +0200

    add ecs client config recipe

--- docker/recipes/ecs-client-config.rb
+++ docker/recipes/ecs-client-config.rb
@@ -1,7 +1,7 @@
 unless node[:docker_registry].nil?
 
   registry = node[:docker_registry][:type]
-  accessData = Chef::JSONCompat.to_json(node[:docker_registry][:auth_data])
+  accessData = Chef::JSONCompat.to_json(node[:docker_registry][:auth_data]).gsub('/', '\/')
 
   Chef::Log.info("Saving access config to ecs config: #{accessData}")
 
@@ -21,7 +21,7 @@ unless node[:docker_registry].nil?
       user "root"
       code <<-EOH
         sed -i -e 's/ECS_ENGINE_AUTH_TYPE=.*/ECS_ENGINE_AUTH_TYPE=#{registry}/' /etc/ecs/ecs.config
-        sed -i -e 's/ECS_ENGINE_AUTH_TYPE=.*/ECS_ENGINE_AUTH_TYPE=#{accessData}/' /etc/ecs/ecs.config
+        sed -i -e 's/ECS_ENGINE_AUTH_DATA=.*/ECS_ENGINE_AUTH_DATA=#{accessData}/' /etc/ecs/ecs.config
       EOH
     end
 
