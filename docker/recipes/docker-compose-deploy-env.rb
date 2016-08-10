29630682cb1cbf81624e3407195b874f1286583c
dklengel <dk@klengel.eu>
2016-08-10 15:33:22 +0200

    remove debug output

--- docker/recipes/docker-compose-deploy-env.rb
+++ docker/recipes/docker-compose-deploy-env.rb
@@ -50,7 +50,6 @@ node[:deploy].each do |application, deploy|
     environment composeEnv
     user "root"
     code <<-EOH
-      printenv >> #{deploy[:deploy_to]}/current/.env
       docker-compose -f #{deploy[:deploy_to]}/current/docker-compose.yml down
       docker-compose -f #{deploy[:deploy_to]}/current/docker-compose.yml up -d --remove-orphans 
     EOH
