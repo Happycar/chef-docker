7a4cdd657041c5c07596576166250f2d93b2cc67
dklengel <dk@klengel.eu>
2016-08-10 16:45:40 +0200

    Update docker-compose-deploy-env.rb

--- docker/recipes/docker-compose-deploy-env.rb
+++ docker/recipes/docker-compose-deploy-env.rb
@@ -45,28 +45,13 @@ node[:deploy].each do |application, deploy|
   
   Chef::Log.info('IMAGE_VERSION set to ' + imageVersion)
   
-  bash "docker-compose pull" do
-    user "root"
-    cwd deploy[:deploy_to] + "/current/"
-      code <<-EOH
-        docker-compose pull
-    EOH
-  end
-
-  bash "docker-compose stop previous" do
-    environment composeEnv
-    user "root"
-    cwd deploy[:deploy_to] + "/releases/"
-    code <<-EOH
-      cd $(ls | sort -r | head -2 | tail -1) && docker-compose down
-    EOH
-  end
-  
-  bash "docker-run" do
+  bash "docker-compose run" do
     environment composeEnv
     user "root"
     cwd deploy[:deploy_to] + "/current/"
     code <<-EOH
+      docker-compose pull
+      docker-compose down
       docker-compose up -d --remove-orphans 
     EOH
   end
