3ab2efe71adc67ce882f49afe373f9eaa9f70f0d
dklengel <dk@klengel.eu>
2016-09-16 18:29:51 +0200

    Update install2.rb

--- docker/recipes/install2.rb
+++ docker/recipes/install2.rb
@@ -1,6 +1,20 @@
+include_recipe 'apt'
+package 'apt-transport-https'
+
 Chef::Log.info(" === :: Docker Install :: === ")
 
-package "docker.io" do
+apt_repository "docker" do
+  uri "https://get.docker.com/ubuntu"
+  distribution "docker"
+  components ["main"]
+  keyserver "hkp://keyserver.ubuntu.com:80"
+  key "36A1D7869245C8950F966E92D8576A8BA88D21E9"
+  not_if { File.exists?("/etc/apt/sources.list.d/docker.list") }
+end
+
+# Install Docker latest version
+package "docker" do
+  package_name "lxc-docker"
   action :install
 end
 
