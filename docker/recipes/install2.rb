9b59b3fd2b8c2bf7b2d6d522380d15ebc81fca14
dklengel <dk@klengel.eu>
2016-09-16 18:40:47 +0200

    Update install2.rb

--- docker/recipes/install2.rb
+++ docker/recipes/install2.rb
@@ -1,24 +1,22 @@
-include_recipe 'apt'
-package 'apt-transport-https'
-
 Chef::Log.info(" === :: Docker Install :: === ")
 
-apt_repository "docker" do
-  uri "https://get.docker.com/ubuntu"
-  distribution "docker"
-  components ["main"]
-  keyserver "hkp://keyserver.ubuntu.com:80"
-  key "36A1D7869245C8950F966E92D8576A8BA88D21E9"
-  not_if { File.exists?("/etc/apt/sources.list.d/docker.list") }
+sourceList = "/etc/apt/sources.list.d/docker.list"
+
+bash "docker-install" do
+  user "root"
+  not_if { ::File.exists?("/usr/bin/docker") }
+  code <<-EOH
+    apt-get update
+    apt-get install apt-transport-https ca-certificates -y
+    apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+    echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> #{/etc/apt/sources.list.d/docker.list}
+    apt-get update
+    apt-get purge lxc-docker -y
+    apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual docker-engine -y
+  EOH
 end
 
-# Install Docker latest version
-package "docker" do
-  package_name "lxc-docker"
-  action :install
-end
-
-Chef::Application.fatal!("Docker is not installed") unless File.exists?("/usr/bin/docker")
+Chef::Application.fatal!("Docker is not installed") unless ::File.exists?("/usr/bin/docker")
 
 service "docker" do
   action :start
