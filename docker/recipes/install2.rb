8f27d74531f704c7f55f887b4bfafa2746eb653d
dklengel <dk@klengel.eu>
2016-09-19 12:15:55 +0200

    Update install2.rb

--- docker/recipes/install2.rb
+++ docker/recipes/install2.rb
@@ -5,14 +5,14 @@ Chef::Log.info(" === :: Docker Install :: === ")
 bash "Install Docker" do
   user "root"
   not_if { File.exists?("/usr/bin/docker") }
-  ignore_failure true
   code <<-EOH
-    apt-get update 
+    apt-get update -y
+    apt-get upgrade -y
     apt-get install apt-transport-https ca-certificates -y 
     apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-    echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> "/etc/apt/sources.list.d/docker.list"
-    apt-get update >> 
-    apt-get purge lxc-docker -y >> 
+    echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > "/etc/apt/sources.list.d/docker.list"
+    apt-get update -y
+    apt-get purge lxc-docker -y
     apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual docker-engine -y 
   EOH
 end
