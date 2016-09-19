3fc8f6efa288287d5d3514b36468b03352ffee2d
dklengel <dk@klengel.eu>
2016-09-19 13:02:41 +0200

    Update install2.rb

--- docker/recipes/install2.rb
+++ docker/recipes/install2.rb
@@ -2,19 +2,14 @@ Chef::Log.level = :debug
 
 Chef::Log.info(" === :: Docker Install :: === ")
 
-bash "Install Docker" do
-  user "root"
-  not_if { File.exists?("/usr/bin/docker") }
-  code <<-EOH
-    apt-get update -y
-    apt-get upgrade -y
-    apt-get install apt-transport-https ca-certificates -y 
-    apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-    echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > "/etc/apt/sources.list.d/docker.list"
-    apt-get update -y
-    apt-get purge lxc-docker -y
-    apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual docker-engine -y 
-  EOH
+bash "docker-install" do
+    user "root"
+    not_if { File.exists?("/usr/bin/docker") }
+    code <<-EOH
+      wget -qO- https://get.docker.com/ | sh
+      export MIRROR_SOURCE=https://registry.hub.docker.com
+      export MIRROR_SOURCE_INDEX=https://registry.hub.docker.com
+    EOH
 end
 
 Chef::Application.fatal!("Docker is not installed") unless File.exists?("/usr/bin/docker")
