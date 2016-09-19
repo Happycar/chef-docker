411767285b37205cae6d44e01b58a51392a0372c
dklengel <dk@klengel.eu>
2016-09-19 10:50:23 +0200

    add more logging

--- docker/recipes/install2.rb
+++ docker/recipes/install2.rb
@@ -2,11 +2,18 @@ Chef::Log.level = :debug
 
 Chef::Log.info(" === :: Docker Install :: === ")
 
-bash "docker-install" do
+logfile = "/tmp/docker-install.log"
+file logfile do
+    action :delete
+end
+
+command = "Install docker"
+bash command do
   user "root"
   not_if { ::File.exists?("/usr/bin/docker") }
   code <<-EOH
-    set -x
+    set -xv
+    set +e
     apt-get update
     apt-get install apt-transport-https ca-certificates -y
     apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
@@ -14,7 +21,18 @@ bash "docker-install" do
     apt-get update
     apt-get purge lxc-docker -y
     apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual docker-engine -y
+    #{command} &> #{logfile}
   EOH
+end
+
+ruby_block "printLog" do
+    only_if { ::File.exists?(logfile) }
+    block do
+        print "\n"
+        File.open(logfile).each do |line|
+            print line
+        end
+    end
 end
 
 Chef::Application.fatal!("Docker is not installed") unless ::File.exists?("/usr/bin/docker")
