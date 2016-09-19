4549107f7952f3242257eeb2c0be1a53b4e2e4c3
dklengel <dk@klengel.eu>
2016-09-19 14:03:13 +0200

    Update install2.rb

--- docker/recipes/install2.rb
+++ docker/recipes/install2.rb
@@ -1,12 +1,12 @@
-Chef::Log.info(" === :: Docker Install :: === ")
-
+Chef::Log.info(" === :: Docker BUILD :: === ")
 bash "docker-install" do
-  user "root"
-  code <<-EOH
-    wget -qO- https://get.docker.com/ | sh
-    export MIRROR_SOURCE=https://registry.hub.docker.com
-    export MIRROR_SOURCE_INDEX=https://registry.hub.docker.com
-  EOH
+user "root"
+#cwd "#{deploy[:deploy_to]}/current"
+code <<-EOH
+  wget -qO- https://get.docker.com/ | sh
+  export MIRROR_SOURCE=https://registry.hub.docker.com
+  export MIRROR_SOURCE_INDEX=https://registry.hub.docker.com
+EOH
 end
 
 service "docker" do
