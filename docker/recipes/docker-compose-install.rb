a444aa1cb6d60aceab52393f0dd624acd1f5f770
dklengel <dk@klengel.eu>
2016-09-19 14:09:00 +0200

    update docker compose from rc to 1.8.0

--- docker/recipes/docker-compose-install.rb
+++ docker/recipes/docker-compose-install.rb
@@ -5,7 +5,7 @@ Chef::Application.fatal!("docker is not installed") unless ::File.exists?("/usr/
 bash "docker-compose-install" do
   user "root"
   code <<-EOH
-    curl -L https://github.com/docker/compose/releases/download/1.8.0-rc1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
   EOH
 end
