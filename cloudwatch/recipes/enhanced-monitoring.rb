0025af4e5bba8a6998ef4c3bf9a9a7825b5d822a
Maik Schmidt <maik.schmidt.hl@gmail.com>
2016-12-07 14:22:18 +0100

    install missing packages and change script path

--- cloudwatch/recipes/enhanced-monitoring.rb
+++ cloudwatch/recipes/enhanced-monitoring.rb
@@ -3,10 +3,11 @@ Chef::Log.info(" === :: CloudWatch - Enhanced Monitoring :: === ")
 bash "cloudwatch-enhanced-monitoring-setup" do
     user "root"
     code <<-EOH
+        apt-get install -y unzip libwww-perl libdatetime-perl
         curl http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip -O
         unzip CloudWatchMonitoringScripts-1.2.1.zip
         rm CloudWatchMonitoringScripts-1.2.1.zip
         touch /var/log/cloudwatch-enhanced-monitoring-cron.log
-        crontab -l | { cat; echo "* * * * * /aws-scripts-mon/mon-put-instance-data.pl --mem-util --swap-util --disk-path=/ --disk-space-util >> /var/log/cloudwatch-enhanced-monitoring-cron.log 2>&1"; } | crontab -
+        crontab -l | { cat; echo "* * * * * ~/aws-scripts-mon/mon-put-instance-data.pl --mem-util --swap-util --disk-path=/ --disk-space-util >> /var/log/cloudwatch-enhanced-monitoring-cron.log 2>&1"; } | crontab -
     EOH
 end
