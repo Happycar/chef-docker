4aa69a8317d5622623ffa3ec2a1e9d4efcb6be0a
Maik Schmidt <maik.schmidt.hl@gmail.com>
2016-12-14 09:35:29 +0100

    add missing metadata

---
+++ ec2/metadata.rb
@@ -0,0 +1,12 @@
+name             "ec2"
+
+maintainer       "Maik Schmidt"
+maintainer_email "maik.schmidt@happycar.de"
+license          "Apache"
+description      "Sets tags on EC2 instances when using OpsWorks"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.0.0"
+
+depends 'aws', '>= 0.2.4'
+
+provides 'ec2::assign-tags'
