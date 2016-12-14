7c32f12ad4dc27e7a5910eb156a7f2fbe52a8cae
Maik Schmidt <maik.schmidt.hl@gmail.com>
2016-12-14 14:17:13 +0100

    using aws 2.9.3

--- ec2/metadata.rb
+++ ec2/metadata.rb
@@ -8,6 +8,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 
 depends 'ohai', '= 3.0.1'
-depends 'aws', '= 2.4.0'
+depends 'aws', '= 2.9.3'
 
 provides 'ec2::assign-tags'
