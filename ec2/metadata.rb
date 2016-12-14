22ce2a4d6b5c61e1b9181c728c8f7ca85ef61736
Maik Schmidt <maik.schmidt.hl@gmail.com>
2016-12-14 10:59:27 +0100

    add ohai depends

--- ec2/metadata.rb
+++ ec2/metadata.rb
@@ -7,6 +7,7 @@ description      "Assign tags on EC2 instances when using OpsWorks"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 
+depends 'ohai', '= 3.0.1'
 depends 'aws', '= 2.9.3'
 
 provides 'ec2::assign-tags'
