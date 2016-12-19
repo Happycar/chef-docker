d6d1feba9d9acb18756e84cc1090030e8a2ddf0a
Maik Schmidt <maik.schmidt.hl@gmail.com>
2016-12-19 13:40:30 +0100

    using pip

--- awscli/recipes/install.rb
+++ awscli/recipes/install.rb
@@ -3,11 +3,7 @@ Chef::Log.info(" === :: AWS CLI - Install :: === ")
 bash "awscli-install" do
     user "root"
     code <<-EOH
-        apt-get install -y unzip
-        cd ~/
-        curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-        unzip awscli-bundle.zip
-        rm awscli-bundle.zip
-        ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+        apt-get install -y python-pip
+        pip install awscli
     EOH
 end
