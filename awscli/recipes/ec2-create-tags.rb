633e3036b81e1f94fec1737abcb7b8d436a39038
Maik Schmidt <maik.schmidt.hl@gmail.com>
2016-12-19 14:10:07 +0100

    revert a34efb46bc387aac11498dae61313c179211d60a

--- awscli/recipes/ec2-create-tags.rb
+++ awscli/recipes/ec2-create-tags.rb
@@ -4,12 +4,10 @@ bash "awscli-ec2-create-tags" do
     user "root"
     code <<-EOH
         TAGS='#{node['awscli']['ec2-create-tags']}'
-        if [ -n "$TAGS" ]; then
-            JSON_TAGS=$(echo ${TAGS} | sed -e 's/=>/:/g')
-            JSON_TAGS=$(echo ${JSON_TAGS} | sed -e 's/"/\\"/g')
-            echo ${TAGS}
-            echo ${JSON_TAGS}
-            aws ec2 create-tags --region #{node['opsworks']['instance']['region']} --resources #{node['opsworks']['instance']['aws_instance_id']} --cli-input-json "$(echo ${JSON_TAGS})"
-        fi
+        JSON_TAGS=$(echo ${TAGS} | sed -e 's/=>/:/g')
+        JSON_TAGS=$(echo ${JSON_TAGS} | sed -e 's/"/\\"/g')
+        echo ${TAGS}
+        echo ${JSON_TAGS}
+        aws ec2 create-tags --region #{node['opsworks']['instance']['region']} --resources #{node['opsworks']['instance']['aws_instance_id']} --cli-input-json "$(echo ${JSON_TAGS})"
     EOH
 end
