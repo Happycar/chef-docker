50e23ec9962a1bc5f27517ae29a18b110bdb1456
dklengel <daniel.klengel@happycar.de>
2017-05-09 10:24:47 +0200

    suppress interactive prompts in loggly script

--- docker/recipes/loggly-config.rb
+++ docker/recipes/loggly-config.rb
@@ -6,7 +6,7 @@ bash "run-config" do
     user "root"
     code <<-EOH
         chmod 777 /configure-linux.sh
-        /configure-linux.sh -a #{node[:loggly][:subdomain]} -t #{node[:loggly][:token]} -p #{node[:loggly][:password]} -u #{node[:loggly][:username]}
+        /configure-linux.sh -s -a #{node[:loggly][:subdomain]} -t #{node[:loggly][:token]} -p #{node[:loggly][:password]} -u #{node[:loggly][:username]}
     EOH
   end
 
