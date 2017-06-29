985f0e5d69194546a3b7f0d57b3b9343c492f820
dklengel <daniel.klengel@happycar.de>
2017-06-29 11:05:04 +0200

    install specific docker version

---
+++ docker/recipes/install-from-repo.rb
@@ -0,0 +1,42 @@
+Chef::Log.info(" === :: Docker INSTALL from repository :: === ")
+bash "docker-install" do
+user "root"
+
+# default docker version to use
+DOCKER_VERSION = "17.06.0~ce-0~ubuntu"
+  
+unless deploy[:environment_variables][:DOCKER_VERSION].nil?
+  DOCKER_VERSION = deploy[:environment_variables][:DOCKER_VERSION]
+end
+
+code <<-EOH
+  apt-get update
+  apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
+    
+  # download dockers official GPG key
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg > docker.gpg
+  
+  # add to keyring
+  apt-key add docker.gpg
+  
+  # setup stable repository
+  sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+   
+  # install docker-ce 
+  apt-get update
+  apt-get install docker-ce=#{DOCKER_VERSION}
+EOH
+end
+
+service "docker" do
+  action :start
+end
