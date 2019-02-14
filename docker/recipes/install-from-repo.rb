3f6a52e425827b98cbfdfe93fba7faf5c49197cd
a.kostiuk <alexey.kostiuk@happycar.de>
2019-02-14 13:19:29 +0100

    improve error reporting

--- docker/recipes/install-from-repo.rb
+++ docker/recipes/install-from-repo.rb
@@ -7,38 +7,50 @@ DOCKER_VERSION = "18.06.1~ce~3-0~ubuntu"
 
 Chef::Log.info(" attempting to install DOCKER_VERSION #{DOCKER_VERSION}")
 
-code <<-EOH
-  apt-get update
-  apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common \
-    linux-image-extra-$(uname -r) \
-    linux-image-extra-virtual
+bash "prepare system" do
+  user "root"
+  code <<-EOH
+    apt-get update
+    apt-get install -y \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        software-properties-common \
+        linux-image-extra-$(uname -r) \
+        linux-image-extra-virtual
+  EOH
+end
 
-  # download dockers official GPG key
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg > docker.gpg
+bash "veirfy  GPG key and add repository" do
+  user "root"
+  code <<-EOH
+    # download dockers official GPG key
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg > docker.gpg
 
-  # verify fingerprint
-  gpg --with-fingerprint docker.gpg | grep "9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88"
+    # verify fingerprint
+    gpg --with-fingerprint docker.gpg | grep "9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88"
 
-  # exit if it doesn't match (as recommended)
-  if [[ $? != 0 ]]; then
+    # exit if it doesn't match (as recommended)
+    if [[ $? != 0 ]]; then
     raise
-  fi
+    fi
 
-  # add to keyring
-  apt-key add docker.gpg
+    # add to keyring
+    apt-key add docker.gpg
 
-  # setup stable repository
-  sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+    # setup stable repository
+    sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+  EOH
+end
 
-  # install docker-ce
+bash "install docker-ce" do
+  user "root"
+  code <<-EOH
+    # install docker-ce
   apt-get update
   apt-get install -y docker-ce=#{DOCKER_VERSION}
-EOH
+  EOH
 end
