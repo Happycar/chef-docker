dc215a881eb61a7d4148fa0cc55f8eda66fa87d1
a.kostiuk <alexey.kostiuk@happycar.de>
2019-02-14 13:07:20 +0100

    fix docker version

--- docker/recipes/install-from-repo.rb
+++ docker/recipes/install-from-repo.rb
@@ -3,10 +3,10 @@ bash "docker-install" do
 user "root"
 
 # default docker version to use
-DOCKER_VERSION = "17.06.0~ce-0~ubuntu"
-  
-Chef::Log.info(" attempting to install DOCKER_VERSION #{DOCKER_VERSION}")  
-  
+DOCKER_VERSION = "18.06.1~ce~3-0~ubuntu"
+
+Chef::Log.info(" attempting to install DOCKER_VERSION #{DOCKER_VERSION}")
+
 code <<-EOH
   apt-get update
   apt-get install -y \
@@ -16,7 +16,7 @@ code <<-EOH
     software-properties-common \
     linux-image-extra-$(uname -r) \
     linux-image-extra-virtual
-    
+
   # download dockers official GPG key
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg > docker.gpg
 
@@ -24,20 +24,20 @@ code <<-EOH
   gpg --with-fingerprint docker.gpg | grep "9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD89"
 
   # exit if it doesn't match (as recommended)
-  if [[ $? != 0 ]]; then 
+  if [[ $? != 0 ]]; then
     raise
   fi
-  
+
   # add to keyring
   apt-key add docker.gpg
-  
+
   # setup stable repository
   sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-   
-  # install docker-ce 
+
+  # install docker-ce
   apt-get update
   apt-get install -y docker-ce=#{DOCKER_VERSION}
 EOH
