89f4514fc354f203a210d4b3ce180b1039d121a9
a.kostiuk <alexey.kostiuk@happycar.de>
2019-05-15 15:57:40 +0200

    use same project name for ease of down commnad

--- docker/recipes/docker-compose-deploy.rb
+++ docker/recipes/docker-compose-deploy.rb
@@ -20,13 +20,13 @@ node[:deploy].each do |application, deploy|
     app application
   end
 
-  unless deploy[:environment_variables].nil? 
+  unless deploy[:environment_variables].nil?
     if ::File.exists?(deploy[:deploy_to] + "/current/docker-compose.yml")
       Chef::Log.info('docker-compose-run start')
 
       deployEnv = deploy[:environment_variables].to_hash
       nodeEnv = node[:environment_variables].to_hash
-  
+
       composeEnv = nodeEnv.merge!(deployEnv)
 
       composeEnv['PRIVATE_IP'] = node[:opsworks][:instance][:private_ip]
@@ -44,7 +44,7 @@ node[:deploy].each do |application, deploy|
         user "root"
         cwd deploy[:deploy_to] + "/releases/"
         code <<-EOH
-          cd $(ls | sort -r | head -2 | tail -1) && docker-compose down
+          cd $(ls | sort -r | head -2 | tail -1) && @docker-compose -p app down || true
         EOH
       end
 
@@ -53,13 +53,13 @@ node[:deploy].each do |application, deploy|
         environment composeEnv
         cwd deploy[:deploy_to] + "/current/"
         code <<-EOH
-          docker-compose up -d
+          docker-compose -p app up -d
         EOH
       end
 
-      # removes all unused images 
-      # exited containers 
-      # unused volumes 
+      # removes all unused images
+      # exited containers
+      # unused volumes
       bash "docker cleanup" do
         user "root"
         code <<-EOH
